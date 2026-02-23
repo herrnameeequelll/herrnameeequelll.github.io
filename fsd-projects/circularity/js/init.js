805dc75ca54573e92ef4b653a2a3bf42bf1476d9
herrnameeequelll <marcquell.gordon@inspire35.org>
2026-02-23 21:57:05 +0000

    made one ghost move

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -27,7 +27,7 @@ var init = function (window) {
         // TODO 2 : Create a function that draws a circle 
         var drawCircle = function() {
 
-            // create the circle 
+            // create the circle
             // randomCircleInArea(area, randomizeAlpha, addCross, borderColor, borderThickness, randomRadialProps)
             circle = draw.randomCircleInArea(canvas, true, true, '#f717e8ff', 22);
 
